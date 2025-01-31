d7f67fd09f59ea7caa64a740355f787aefc20cb3
HP <HP@DESKTOP-9CA997L.mshome.net>
2025-01-31 13:07:21 +0530

    changes

--- newbuild/src/test/java/newframework/NewCode.java
+++ newbuild/src/test/java/newframework/NewCode.java
@@ -5,8 +5,18 @@ import org.testng.annotations.Test;
 public class NewCode {
 
 	@Test
-	public void runner()
+	public void createContactTest()
 	{
-		System.out.println("hi world");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("exexute createContactTest");
+		
 	}
 }
